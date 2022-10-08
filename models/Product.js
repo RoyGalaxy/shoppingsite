5b2285fe6d992b097bb18e30b4e1c779339a95e5
RoyGalaxy <ajroysstyle@gmail.com>
2022-10-09 00:57:18 +0530

    added dashboard and backend

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const OrderSchema = new mongoose.Schema(
+    {
+        name:{type:String,required:true,unique:true},
+        description:{type:String,default:""},
+        image:{type:String,required:true},
+        category:{type:String,required:true},
+        price:{type:Number,required:true},
+        model3d:{type:String,required:true}
+    },
+    {timestamps:true}
+)
+
+module.exports = mongoose.model("Product",OrderSchema)
