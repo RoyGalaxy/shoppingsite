8c46b4f9268d0b59ad1346eb3f31a223c5ca6c8d
Roy Galaxy <ajroysstyle@gmail.com>
2022-11-17 10:31:29 +0000

    restored assets folder

---
+++ public/assets/js/apiRequestHandler.js
@@ -0,0 +1,44 @@
+const getProduct = async (id) => {
+    const res = await fetch(`/api/products/find/${id}`)
+    const jsonRes = await res.json()
+    let product = await jsonRes
+
+    return await product
+}
+
+const getAllProducts = async () => {
+    const res = await fetch(`/api/products/`)
+    const jsonRes = await res.json()
+    let products = await jsonRes
+
+    return await products
+}
+
+const getUser = async (id) => {
+    const user = JSON.parse(localStorage.user)
+    const res = await fetch(`/api/users/find/${id}`,
+    {
+        headers: {token: `Bearer ${user.accessToken}`},
+    })
+    const jsonRes = await res.json()
+    let userRes = await jsonRes
+
+    return await userRes
+}
+
+const updateOrder = async(orderId,update) => {
+    const user = JSON.parse(localStorage.user)
+    const res = await fetch(`/api/orders/${orderId}`,
+        {
+            method: "put",
+            body: JSON.stringify(update),
+            headers: {
+                "content-type": "application/json",
+                token: `Bearer ${user.accessToken}`
+            },
+        })
+    const jsonRes = await res.json()
+    let userRes = await jsonRes
+
+    return await userRes
+}
