b7d883726d74d19eb486bc12651e4230aa749946
RoyGalaxy <ajroysstyle@gmail.com>
2023-06-06 01:34:59 +0530

    require module error fixed

--- routes/colorScheme.js
+++ routes/colorScheme.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const {verifyTokenAndAdmin} = require("./verifyToken")
 const path = require("path")
 const fs = require("fs")
-const hexToHsl = require("")
+// const hexToHsl = require("")
 
 // :root{
 //     --primary: hsl(0, 0%, 100%);
